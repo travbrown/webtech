2918f59ded433fb42fdbfd5643f96859ca026095
Travis Brown <travjbrown98@gmail.com>
2019-09-17 17:27:48 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/gaza', (req, res) => res.send('GAZA GAZA GAZA!'));
+
+app.listen(port, () => console.log('Eg app listening on port ${port}!'));
---
+++ index.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+
+/*console.log() these:
+os, os.cpus(), os.cpus().length
+os.totalmem() - this is in bytes
+os.freemem()
+//console.log(os.totalmem()/(1024*3) + ' GBs');
+*/
+
+console.log(fs);
