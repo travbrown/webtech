d3322c4a0cfa6df398c0019008e34876930e9a90
Travis Brown <travjbrown98@gmail.com>
2019-09-18 22:48:13 +0000

    negligible updates

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/gaza', (req, res) => res.send('GAZA GAZA GAZA!'));
 
+app.post();
+
 app.listen(port, () => console.log('Eg app listening on port ${port}!'));
--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-let fs = require("fs");
-
-/*console.log() these:
-os, os.cpus(), os.cpus().length
-os.totalmem() - this is in bytes
-os.freemem()
-//console.log(os.totalmem()/(1024*3) + ' GBs');
-*/
-
-console.log(fs);
+console.log('Godwin likes buying sugar bread.');
