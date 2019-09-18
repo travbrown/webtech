717540d224c50b2ce431951d8d11616646d024d8
Keith Chan <keith.chan21@gmail.com>
2019-09-18 21:02:23 +0000

    completed with basic functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// let fs = require("fs");
-import 'dotenv/config';
-
 /*
+ * IMPORTANT FILES AND LIBRARIES
+ *
  * Nodemon is a library that keeps our script running, i.e. we do not need to re-enter npm start 
  * each time we make a change
  *
@@ -9,8 +8,127 @@ import 'dotenv/config';
  * JS features that may not exist in Node.js yet. Change the .babelrc file to activate different
  * JS features
  * 
+ * CORS allows cross-library resource sharing
+ * uuid is used to generate unique IDs
+ *
  * Open the .env file to store sensitive data separate from the source code
  * console.log(process.env.MY_SECRET);
  */
-console.log('Node.js Project running Express, Nodemon, Babel');
-//console.log(fs);
+
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import uuidv4 from 'uuid/v4';
+import models from './models';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Get sample data from models/index.js
+app.use((req, res, next) => {
+	req.context = {
+		models,
+		me: models.users[1],
+	};
+	next();
+});
+
+/******* ENDPOINTS *******/
+
+// GET
+
+app.get('/', (req, res) => {
+  return res.send('Hello World!');
+});
+
+app.get('/session', (req, res) => {
+	return res.send(req.context.models.users[req.context.me.id]);
+})
+
+app.get('/users', (req, res) => {
+  return res.send(Object.values(req.context.models.users));
+});
+
+app.get('/users/:userId', (req, res) => {
+  return res.send(req.context.models.users[req.params.userId]);
+});
+
+app.get('/messages', (req, res) => {
+  return res.send(Object.values(req.context.models.messages));
+});
+
+app.get('/messages/:messageId', (req, res) => {
+  return res.send(req.context.models.messages[req.params.messageId]);
+});
+
+// POST
+
+app.post('/users', (req, res) => {
+	if(!req.body.username) {
+		return res.status(400).send({
+			success: 'false',
+			message: 'no username provided',
+		});
+	};
+	const id = Object.keys(req.context.models.users).length + 1;
+	const user = {
+		id,
+		username: req.body.username,
+	};
+	req.context.models.users[id] = user;
+	return res.send(user);
+});
+
+app.post('/messages', (req, res) => {
+	if(!req.body.text) {
+		return res.status(400).send({
+			success: 'false',
+			message: 'no text provided',
+		});
+	} else if (!req.body.to) {
+		return res.status(400).send({
+			success: 'false',
+			message: 'no to address provided',
+		});
+	}
+	const messageId = uuidv4();
+	const message = {
+		messageId,
+		text: req.body.text,
+		to: req.body.to,
+		from: req.context.me.id,
+	};
+	req.context.models.messages[messageId] = message;
+	return res.send(message);
+});
+
+// PUT
+
+app.put('/users/:userId', (req, res) => {
+  return res.send(
+    `PUT HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+// DELETE
+
+app.delete('/users/:userId', (req, res) => {
+  return res.send(
+    `DELETE HTTP method on user/${req.params.userId} resource`,
+  );
+});
+
+app.delete('/messages/:messageId', (req, res) => {
+  const {
+    [req.params.messageId]: message,
+    ...otherMessages
+  } = req.context.models.messages;
+  req.context.models.messages = otherMessages;
+  return res.send(message);
+});
+
+// Print to command line when app starts up
+app.listen(process.env.PORT, () => 
+	console.log(`App listening on port ${process.env.PORT}!`));
---
+++ src/models/index.js
@@ -0,0 +1,31 @@
+// Sample Data
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Remus',
+  },
+  2: {
+    id: '2',
+    username: 'Demi',
+  },
+};
+let messages = {
+  1: {
+    messageId: '1',
+    text: 'Hello World',
+    to: '2',
+    from: '1',
+  },
+  2: {
+    messageId: '2',
+    text: 'By World',
+    to: '2',
+    from: '1',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
